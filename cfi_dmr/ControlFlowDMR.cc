3c5a87141e9391bfd4114408e9b77171c89da619
Haoda Wang <haoda.wang@jpl.nasa.gov>
2023-07-21 09:27:31 -0700

    Check for uses instead of manually looping

--- cfi_dmr/ControlFlowDMR.cc
+++ cfi_dmr/ControlFlowDMR.cc
@@ -14,87 +14,67 @@
 namespace ControlFlowDMR {
 llvm::PreservedAnalyses ControlFlowDMRPass::run(llvm::Function &F,
                                                 llvm::FunctionAnalysisManager &AM) {
-  std::unordered_set<llvm::Value *> important_values;
-  std::unordered_set<llvm::Instruction *>terminator_values;
-  std::stack<llvm::Value *> important_values_stack;
+  std::unordered_set<llvm::Instruction *> important_insns;
+  std::unordered_set<llvm::Instruction *> terminator_insns;
+  std::stack<llvm::Instruction *> important_insns_stack;
 
   // Go through all BBs and record each terminator value
-  for (llvm::Function::iterator bb_it = F.begin(); bb_it != F.end(); ++bb_it) {
-    llvm::BasicBlock *bb = &*bb_it;
-
-    terminator_values.insert(bb->getTerminator());
+  for (auto &bb : F) {
+    terminator_insns.insert(bb.getTerminator());
   }
 
   // Get important values for each terminator value
-  for (llvm::Instruction *terminator : terminator_values) {
-    important_values.insert(terminator);
+  for (llvm::Instruction *terminator : terminator_insns) {
+    important_insns.insert(terminator);
 
-    // Add all terminator operands into "important operands list"
-    if (auto u_br = dyn_cast<llvm::BranchInst>(terminator)) {
-      if (u_br->isConditional())
-        important_values.insert(u_br->getCondition());
-    } else {
-      for (int i = 0; i < terminator->getNumOperands(); i++)
-        important_values.insert(terminator->getOperand(i));
+    // Add all terminator users into "important operands list"
+    for (llvm::Use &u : terminator->operands()) {
+      llvm::Value *v = u.get();
+
+      if (auto v_insn = dyn_cast<llvm::Instruction>(v))
+        important_insns_stack.push(v_insn);
     }
   }
 
-  for (llvm::Value *value : important_values)
-    important_values_stack.push(value);
+  for (llvm::Instruction *insn : important_insns)
+    important_insns_stack.push(insn);
 
   // Go through the uses of each important operand and insert all values into
   // the use-def tree
-  while (!important_values_stack.empty()) {
-    llvm::Value *important_value = important_values_stack.top();
-    important_values_stack.pop();
+  while (!important_insns_stack.empty()) {
+    llvm::Instruction *important_insn = important_insns_stack.top();
+    important_insns_stack.pop();
 
-    if (important_value == nullptr || important_values.contains(important_value))
+    if (important_insn == nullptr || important_insns.contains(important_insn))
       continue;
     else
-      important_values.insert(important_value);
+      important_insns.insert(important_insn);
 
-    // Handle instructions that may be called by previous instructions
-    if (auto br_insn = dyn_cast<llvm::BranchInst>(important_value)) {
-      // It's a branch instruction, so we add the condition to the list
-      if (br_insn->isConditional())
-        important_values_stack.push(br_insn->getCondition());
-    } else if (auto alloc_insn = dyn_cast<llvm::AllocaInst>(important_value)) {
-      // It's an alloc instruction, so we add the size to the list if it isn't
-      // a static allocation
-      if (!alloc_insn->isStaticAlloca())
-        important_values_stack.push(alloc_insn->getArraySize());
-    }  else if (auto insn = dyn_cast<llvm::Instruction>(important_value)) {
-      // It's another instruction, so we just add the operands to the list
-      for (int i = 0; i < insn->getNumOperands(); i++)
-        important_values_stack.push(insn->getOperand(i));
+    // Traverse use-def tree and push in other important values
+    for (llvm::Use &u : important_insn->operands()) {
+      llvm::Value *v = u.get();
+
+      if (auto v_insn = dyn_cast<llvm::Instruction>(v))
+        important_insns_stack.push(v_insn);
     }
   }
 
-  llvm::outs() << "\nimportant_values:\n";
-  for (llvm::Value *important_value : important_values) {
-    if (auto important_insn = dyn_cast<llvm::Instruction>(important_value))
-      llvm::outs() << "  insn $" << important_insn->getName() << ": "
-                   << important_value << "\n";
-    else
-      llvm::outs() << "  const $" << important_value->getValueID() << ": "
-                   << important_value << "\n";
+  llvm::outs() << "\nimportant_insns:\n";
+  for (llvm::Value *important_insn : important_insns) {
+    llvm::outs() << "  insn $" << important_insn->getName() << ": "
+                 << important_insn << "\n";
   }
-
 
   // Go through all BBs and remove non-important values
-  for (llvm::Function::iterator bb_it = F.begin(); bb_it != F.end(); ++bb_it) {
-    llvm::BasicBlock *bb = &*bb_it;
+  for (auto &bb : F) {
     std::vector<llvm::Instruction *> insn_list;
 
-    for (llvm::BasicBlock::iterator insn_it = bb->begin(); insn_it != bb->end(); insn_it++) {
-      llvm::Instruction *insn = &*insn_it;
-      insn_list.push_back(insn);
-    }
+    for (auto &insn: bb)
+      insn_list.push_back(&insn);
 
-    for (auto insn : insn_list) {
-      if (!important_values.contains(insn))
+    for (auto insn: insn_list)
+      if (!important_insns.contains(insn))
         insn->eraseFromParent();
-    }
   }
 
   return llvm::PreservedAnalyses::all();
