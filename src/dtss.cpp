b43e0b989f90d9a1161945db63b9f5b4a0b5f15a
Haoda Wang <haoda.wang@jpl.nasa.gov>
2024-02-13 12:58:04 -0600

    Add a default conflict matching function

--- src/dtss.cpp
+++ src/dtss.cpp
@@ -1,14 +1,58 @@
 #include "dtss.h"
+
+#include <map>
 #include <thread>
 
-int rad_hard_compute(OutputData *output_format,
-                     std::unordered_set<InputData *> (*partitioner)(),
-                     OutputData (*processor)(InputData *)) {
-  // Partitioner creates input data and assigns conflicts for each dataset
-  auto input_set = (*partitioner)();
+void DTSSInstance::build_conflicts_list(
+    std::unordered_set<InputData *> &input_data) {
+  std::multimap<size_t, InputData *> memory_regions;
+
+  // Build a map of all the memory regions that are being built
+  for (auto input : input_data) {
+    // Insert an interval at each start and end of the memory region
+    memory_regions.insert(
+        std::pair<size_t, InputData *>((size_t)(input->data_ptr), input));
+    memory_regions.insert(std::pair<size_t, InputData *>(
+        (size_t)(input->data_ptr) + input->data_size, input));
+  }
+
+  // Iterate through memory intervals and log intersections in conflicts
+  std::unordered_set<InputData *> active_regions;
+  for (auto it = memory_regions.begin(); it != memory_regions.end(); ++it) {
+    bool is_original = active_regions.insert(it->second).second;
+
+    // Insert all currently active regions into conflict graph
+    for (auto input1 : active_regions)
+      for (auto input2 : active_regions)
+        if (input1 != input2)
+          this->conflicts[input1].insert(input2);
+
+    // If this is the 2nd time we see it, it's the end of the memory region of
+    // this dataset
+    if (!is_original)
+      active_regions.erase(it->second);
+  }
+}
+
+int DTSSInstance::dtss_compute(OutputData *output_format,
+                               std::unordered_set<InputData *> dataset,
+                               OutputData (*processor)(InputData *)) {
+  // Default artitioner creates input data and assigns conflicts
+  this->build_conflicts_list(dataset);
 
   // TODO: scheduling for the thing, based on conflicts
-  // TODO: N-coloring to schedule? but how do with rad hardening
+  // TODO: N-coloring to schedule?
+
+  return 0;
+}
+
+int DTSSInstance::dtss_compute(
+    OutputData *output_format,
+    std::unordered_map<InputData *, std::unordered_set<InputData *>> (
+        *partitioner)(),
+    OutputData (*processor)(InputData *)) {
+  // Partitioner function creates input data and assigns conflicts
+  conflicts = partitioner();
 
   return 0;
 }
--- src/dtss.h
+++ src/dtss.h
@@ -1,15 +1,12 @@
 #ifndef DTSS_H
 #define DTSS_H
 
+#include <unordered_map>
 #include <unordered_set>
 
 struct InputData {
   size_t data_size;
-  void* data_ptr;
-
-  std::unordered_set<InputData *> conflicts;
-
-  virtual ~InputData() {}
+  void *data_ptr;
 };
 
 struct OutputData {
@@ -17,8 +14,21 @@ struct OutputData {
   virtual ~OutputData() {}
 };
 
-int dtss_compute(OutputData *output_format,
-                 std::unordered_set<InputData *> (*partitioner)(),
-                 OutputData (*processor)(InputData *));
+class DTSSInstance {
+private:
+  std::unordered_map<InputData *, std::unordered_set<InputData *>> conflicts;
+
+  void build_conflicts_list(std::unordered_set<InputData *> &input_data);
+
+public:
+  int dtss_compute(OutputData *output_format,
+                   std::unordered_set<InputData *> dataset,
+                   OutputData (*processor)(InputData *));
+  int dtss_compute(
+      OutputData *output_format,
+      std::unordered_map<InputData *, std::unordered_set<InputData *>> (
+          *partitioner)(),
+      OutputData (*processor)(InputData *));
+};
 
 #endif
