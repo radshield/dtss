8cd212c57e5d49f5f1b31f153348f21341617578
Haoda Wang <haoda.wang@jpl.nasa.gov>
2024-02-28 13:57:59 -0500

    Try AES encryption

---
+++ src/dtss_crypt.cpp
@@ -0,0 +1,227 @@
+#include <atomic>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <openssl/evp.h>
+#include <thread>
+#include <vector>
+#include <x86intrin.h>
+
+struct InputData {
+public:
+  uint8_t *key, *iv;
+  uint8_t *in, *out;
+};
+
+std::atomic_bool jobs_done = false;
+
+void encrypt_data(InputData &input) {
+  uint8_t iv[16] = {0};
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+  int outlen1, outlen2;
+
+  EVP_EncryptInit(ctx, EVP_aes_256_ecb(), input.key, iv);
+  EVP_EncryptUpdate(ctx, input.out, &outlen1, input.in, 1024);
+  EVP_EncryptFinal(ctx, input.out + outlen1, &outlen2);
+}
+
+void worker_process(boost::lockfree::spsc_queue<InputData *> *job_queue) {
+  while (!job_queue->empty() || !jobs_done) {
+    if (job_queue->empty())
+      continue;
+    else {
+      // Process is in job queue, remove from queue and process
+      InputData *input = job_queue->front();
+      job_queue->pop();
+      encrypt_data(*input);
+      delete input;
+    }
+  }
+}
+
+void clear_cache(std::vector<uint8_t *> &input_data) {
+  for (auto input : input_data) {
+    for (int i = 0; i <= 1024; i += 64) {
+      _mm_clflush(input + i);
+    }
+  }
+}
+
+void orchestrator_process(std::vector<uint8_t *> &input_data) {
+  boost::lockfree::spsc_queue<InputData *> jobqueue_0(128);
+  boost::lockfree::spsc_queue<InputData *> jobqueue_1(128);
+  boost::lockfree::spsc_queue<InputData *> jobqueue_2(128);
+
+  std::vector<uint8_t *> output_data_0;
+  std::vector<uint8_t *> output_data_1;
+  std::vector<uint8_t *> output_data_2;
+
+  for (int i = 0; i < input_data.size() - input_data.size() % 3; i++) {
+    output_data_0.push_back((uint8_t *)malloc(1040));
+    output_data_1.push_back((uint8_t *)malloc(1040));
+    output_data_2.push_back((uint8_t *)malloc(1040));
+  }
+
+  // Set up key and IV
+  uint8_t key[32] = ".rPUkt=4;4*2c1Mk6Zk9L0p09)MA=3k";
+
+  std::chrono::steady_clock::time_point begin =
+      std::chrono::steady_clock::now();
+
+  // Start threads
+  std::thread tmr_0(worker_process, &jobqueue_0);
+  std::thread tmr_1(worker_process, &jobqueue_1);
+  std::thread tmr_2(worker_process, &jobqueue_2);
+
+  // Distribute #1
+  for (int i = 0; i < output_data_0.size(); i += 3) {
+    auto in_0 = new InputData;
+    auto in_1 = new InputData;
+    auto in_2 = new InputData;
+
+    in_0->key = key;
+    in_0->in = input_data[i];
+    in_0->out = output_data_0[i];
+
+    in_1->key = key;
+    in_1->in = input_data[i + 1];
+    in_1->out = output_data_1[i + 1];
+
+    in_2->key = key;
+    in_2->in = input_data[i + 2];
+    in_2->out = output_data_2[i + 2];
+
+    jobqueue_0.push(in_0);
+    jobqueue_1.push(in_1);
+    jobqueue_2.push(in_2);
+  }
+
+  // Wait for compute to end
+  while (!(jobqueue_0.empty() && jobqueue_1.empty() && jobqueue_2.empty()))
+    continue;
+
+  // Clear cache
+  clear_cache(input_data);
+
+  // Distribute #2
+  for (int i = 0; i < output_data_0.size(); i += 3) {
+    auto in_0 = new InputData;
+    auto in_1 = new InputData;
+    auto in_2 = new InputData;
+
+    in_0->key = key;
+    in_0->in = input_data[i + 1];
+    in_0->out = output_data_0[i + 1];
+
+    in_1->key = key;
+    in_1->in = input_data[i + 2];
+    in_1->out = output_data_1[i + 2];
+
+    in_2->key = key;
+    in_2->in = input_data[i];
+    in_2->out = output_data_2[i];
+
+    jobqueue_0.push(in_0);
+    jobqueue_1.push(in_1);
+    jobqueue_2.push(in_2);
+  }
+
+  // Wait for compute to end
+  while (!(jobqueue_0.empty() && jobqueue_1.empty() && jobqueue_2.empty()))
+    continue;
+
+  // Clear cache
+  clear_cache(input_data);
+
+  // Distribute #3
+  for (int i = 0; i < output_data_0.size(); i += 3) {
+    auto in_0 = new InputData;
+    auto in_1 = new InputData;
+    auto in_2 = new InputData;
+
+    in_0->key = key;
+    in_0->in = input_data[i + 2];
+    in_0->out = output_data_0[i + 2];
+
+    in_1->key = key;
+    in_1->in = input_data[i];
+    in_1->out = output_data_1[i];
+
+    in_2->key = key;
+    in_2->in = input_data[i + 1];
+    in_2->out = output_data_2[i + 1];
+
+    jobqueue_0.push(in_0);
+    jobqueue_1.push(in_1);
+    jobqueue_2.push(in_2);
+  }
+
+  // All jobs pushed, send signal to end after compute done
+  jobs_done = true;
+
+  // Wait for threads to end
+  tmr_0.join();
+  tmr_1.join();
+  tmr_2.join();
+
+  // Compare data
+  int count = 0;
+  for (int i = 0; i < output_data_0.size(); i++) {
+    if (memcmp(output_data_0[i], output_data_1[i], 1024) == 0) {
+      // 2 match, assume good
+    } else if (memcmp(output_data_0[i], output_data_2[i], 1024) == 0) {
+      // 2 match, assume good
+    } else if (memcmp(output_data_1[i], output_data_2[i], 1024) == 0) {
+      // 2 match, assume good
+    } else {
+      count++;
+    }
+  }
+
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+  std::cout << "Runtime: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end -
+                                                                     begin)
+                   .count()
+            << " µs" << std::endl;
+  std::cout << count << " / " << output_data_0.size() << std::endl;
+
+  // Cleanup data
+  for (int i = 0; i < input_data.size() - input_data.size() % 3; i++) {
+    free(output_data_0[i]);
+    free(output_data_1[i]);
+    free(output_data_2[i]);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  char *buf;
+  std::vector<uint8_t *> input_data;
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " FILENAME" << std::endl;
+    return -1;
+  }
+
+  std::ifstream ifs(argv[1], std::ios::in | std::ios::binary);
+
+  buf = (char *)malloc(1024);
+  while (ifs.read(buf, 1024)) {
+    memset(buf, 0, 1024);
+    input_data.push_back((uint8_t *)malloc(1024));
+    memcpy(input_data.back(), buf, 1024);
+  }
+  free(buf);
+
+  orchestrator_process(input_data);
+
+  for (uint8_t *input : input_data)
+    free(input);
+
+  return 0;
+}
