31232dfde7f6851d1ec1c4b2b7bedcdb79209ddd
Haoda Wang <haoda.wang@jpl.nasa.gov>
2024-03-25 21:56:03 -0400

    Start working on OpenCV NCC score

---
+++ img/dtss_img.cpp
@@ -0,0 +1,260 @@
+#include "img.h"
+
+#include <atomic>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <pthread.h>
+#include <sched.h>
+#include <thread>
+#include <vector>
+#include <x86intrin.h>
+
+struct InputData {
+public:
+  uint8_t *key;
+  uint8_t *in, *out;
+};
+
+std::atomic_bool jobs_done = false;
+
+void worker_process(boost::lockfree::spsc_queue<InputData *> *job_queue) {
+  while (!job_queue->empty() || !jobs_done) {
+    if (job_queue->empty())
+      continue;
+    else {
+      // Process is in job queue, remove from queue and process
+      InputData *input = job_queue->front();
+      encrypt_data(input->key, input->in, input->out);
+      job_queue->pop();
+      delete input;
+    }
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  long long tmp_count;
+  uint8_t key[256] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+                      0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+                      0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33,
+                      0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31};
+  cpu_set_t cpuset;
+  int r;
+  std::chrono::steady_clock::time_point begin, end, read_begin, read_end,
+      malloc_begin, malloc_end;
+  std::vector<std::chrono::steady_clock::time_point> encrypt_begin(3),
+      encrypt_end(3), cache_begin(2), cache_end(2);
+
+  std::vector<uint8_t *> input_data;
+  std::vector<std::vector<uint8_t *>> output_data(3);
+
+  boost::lockfree::spsc_queue<InputData *> jobqueue_0(1000000);
+  boost::lockfree::spsc_queue<InputData *> jobqueue_1(1000000);
+  boost::lockfree::spsc_queue<InputData *> jobqueue_2(1000000);
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " FILENAME" << std::endl;
+    return -1;
+  }
+
+  begin = std::chrono::steady_clock::now();
+
+  read_begin = std::chrono::steady_clock::now();
+  read_data(argv[1], input_data);
+  read_end = std::chrono::steady_clock::now();
+
+  malloc_begin = std::chrono::steady_clock::now();
+  for (int i = 0; i < input_data.size() - input_data.size() % 3; i++) {
+    output_data[0].push_back((uint8_t *)malloc(CHUNK_SZ + 16));
+    output_data[1].push_back((uint8_t *)malloc(CHUNK_SZ + 16));
+    output_data[2].push_back((uint8_t *)malloc(CHUNK_SZ + 16));
+  }
+  malloc_end = std::chrono::steady_clock::now();
+
+  // Start threads
+  std::thread tmr_0(worker_process, &jobqueue_0);
+  std::thread tmr_1(worker_process, &jobqueue_1);
+  std::thread tmr_2(worker_process, &jobqueue_2);
+
+  CPU_ZERO(&cpuset);
+  CPU_SET(1, &cpuset);
+  r = pthread_setaffinity_np(tmr_0.native_handle(), sizeof(cpu_set_t), &cpuset);
+  if (r != 0)
+    std::cerr << "Error binding worker 0 to core" << std::endl;
+
+  CPU_ZERO(&cpuset);
+  CPU_SET(2, &cpuset);
+  r = pthread_setaffinity_np(tmr_1.native_handle(), sizeof(cpu_set_t), &cpuset);
+  if (r != 0)
+    std::cerr << "Error binding worker 1 to core" << std::endl;
+
+  CPU_ZERO(&cpuset);
+  CPU_SET(3, &cpuset);
+  r = pthread_setaffinity_np(tmr_2.native_handle(), sizeof(cpu_set_t), &cpuset);
+  if (r != 0)
+    std::cerr << "Error binding worker 2 to core" << std::endl;
+
+  encrypt_begin[0] = std::chrono::steady_clock::now();
+
+  // Distribute #1
+  for (int i = 0; i < output_data[0].size(); i += 3) {
+    auto in_0 = new InputData;
+    auto in_1 = new InputData;
+    auto in_2 = new InputData;
+
+    in_0->key = key;
+    in_0->in = input_data[i];
+    in_0->out = output_data[0][i];
+
+    in_1->key = key;
+    in_1->in = input_data[i + 1];
+    in_1->out = output_data[1][i + 1];
+
+    in_2->key = key;
+    in_2->in = input_data[i + 2];
+    in_2->out = output_data[2][i + 2];
+
+    jobqueue_0.push(in_0);
+    jobqueue_1.push(in_1);
+    jobqueue_2.push(in_2);
+  }
+
+  // Wait for compute to end
+  while (!(jobqueue_0.empty() && jobqueue_1.empty() && jobqueue_2.empty()))
+    continue;
+
+  encrypt_end[0] = std::chrono::steady_clock::now();
+
+  // Clear cache
+  cache_begin[0] = std::chrono::steady_clock::now();
+  clear_cache(input_data);
+  cache_end[0] = std::chrono::steady_clock::now();
+
+  encrypt_begin[1] = std::chrono::steady_clock::now();
+
+  // Distribute #2
+  for (int i = 0; i < output_data[0].size(); i += 3) {
+    auto in_0 = new InputData;
+    auto in_1 = new InputData;
+    auto in_2 = new InputData;
+
+    in_0->key = key;
+    in_0->in = input_data[i + 1];
+    in_0->out = output_data[0][i + 1];
+
+    in_1->key = key;
+    in_1->in = input_data[i + 2];
+    in_1->out = output_data[1][i + 2];
+
+    in_2->key = key;
+    in_2->in = input_data[i];
+    in_2->out = output_data[2][i];
+
+    jobqueue_0.push(in_0);
+    jobqueue_1.push(in_1);
+    jobqueue_2.push(in_2);
+  }
+
+  // Wait for compute to end
+  while (!(jobqueue_0.empty() && jobqueue_1.empty() && jobqueue_2.empty()))
+    continue;
+
+  encrypt_end[1] = std::chrono::steady_clock::now();
+
+  // Clear cache
+  cache_begin[1] = std::chrono::steady_clock::now();
+  clear_cache(input_data);
+  cache_end[1] = std::chrono::steady_clock::now();
+
+  encrypt_begin[2] = std::chrono::steady_clock::now();
+
+  // Distribute #3
+  for (int i = 0; i < output_data[0].size(); i += 3) {
+    auto in_0 = new InputData;
+    auto in_1 = new InputData;
+    auto in_2 = new InputData;
+
+    in_0->key = key;
+    in_0->in = input_data[i + 2];
+    in_0->out = output_data[0][i + 2];
+
+    in_1->key = key;
+    in_1->in = input_data[i];
+    in_1->out = output_data[1][i];
+
+    in_2->key = key;
+    in_2->in = input_data[i + 1];
+    in_2->out = output_data[2][i + 1];
+
+    jobqueue_0.push(in_0);
+    jobqueue_1.push(in_1);
+    jobqueue_2.push(in_2);
+  }
+
+  // All jobs pushed, send signal to end after compute done
+  jobs_done = true;
+
+  // Wait for threads to end
+  tmr_0.join();
+  tmr_1.join();
+  tmr_2.join();
+
+  encrypt_end[2] = std::chrono::steady_clock::now();
+
+  // Compare data
+  int count = diff_data(output_data);
+
+  end = std::chrono::steady_clock::now();
+
+  std::cout << count << " / " << output_data[0].size() << std::endl
+            << std::endl;
+
+  std::cout << "Total runtime: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end -
+                                                                     begin)
+                   .count()
+            << " us" << std::endl;
+
+  std::cout << "Disk read runtime: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(read_end -
+                                                                     read_begin)
+                   .count()
+            << " us" << std::endl;
+
+  std::cout << "Malloc runtime: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(
+                   malloc_end - malloc_begin)
+                   .count()
+            << " us" << std::endl;
+
+  tmp_count = 0;
+  for (int i = 0; i < 3; i++) {
+    tmp_count += std::chrono::duration_cast<std::chrono::microseconds>(
+                     encrypt_end[i] - encrypt_begin[i])
+                     .count();
+  }
+  std::cout << "Encrypt runtime: " << tmp_count << " us" << std::endl;
+
+  tmp_count = 0;
+  for (int i = 0; i < 2; i++) {
+    tmp_count += std::chrono::duration_cast<std::chrono::microseconds>(
+                     cache_end[i] - cache_begin[i])
+                     .count();
+  }
+  std::cout << "Cache clear runtime: " << tmp_count << " us" << std::endl
+            << std::endl;
+
+  // Cleanup data
+  for (int i = 0; i < output_data[0].size() % 3; i++) {
+    free(output_data[0][i]);
+    free(output_data[1][i]);
+    free(output_data[2][i]);
+  }
+
+  for (uint8_t *input : input_data)
+    free(input);
+
+  return 0;
+}
---
+++ img/img.h
@@ -0,0 +1,74 @@
+#ifndef IMG_H
+#define IMG_H
+
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <fstream>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <vector>
+#include <x86intrin.h>
+
+#define CHUNK_SZ 8192
+
+int nccscore_data(cv::Mat *img, cv::Mat *match, size_t start_x,
+                  size_t start_y) {
+  int score = 0;
+
+  for (size_t i = 0; i < match->rows; i++) {
+    for (size_t j = 0; j < match->cols; j++) {
+      // Make sure everything is in bounds
+      if (img->rows < start_x + match->rows ||
+          img->cols < start_y + match->cols)
+        break;
+
+      for (size_t k = 0; k < 3; k++) {
+        score += img->at<cv::Vec3b>(start_x + i, start_y + j)[k] -
+                 match->at<cv::Vec3b>(i, j)[k];
+      }
+    }
+  }
+}
+
+void clear_cache(std::vector<uint8_t *> &input_data) {
+  for (auto input : input_data) {
+    for (int i = 0; i <= CHUNK_SZ; i += 64) {
+      _mm_clflush(input + i);
+    }
+  }
+}
+
+void clear_cache_disk() {
+  int fd;
+  std::string data = "3";
+
+  sync();
+  fd = open("/proc/sys/vm/drop_caches", O_WRONLY);
+  write(fd, data.c_str(), sizeof(char));
+  fsync(fd);
+  close(fd);
+}
+
+int diff_data(std::vector<std::vector<uint8_t *>> &output_data) {
+  int count = 0;
+
+  for (int i = 0; i < output_data[0].size(); i++) {
+    if (memcmp(output_data[0][i], output_data[1][i], CHUNK_SZ) == 0) {
+      // 2 match, assume good
+    } else if (memcmp(output_data[0][i], output_data[2][i], CHUNK_SZ) == 0) {
+      // 2 match, assume good
+    } else if (memcmp(output_data[1][i], output_data[2][i], CHUNK_SZ) == 0) {
+      // 2 match, assume good
+    } else {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+#endif
